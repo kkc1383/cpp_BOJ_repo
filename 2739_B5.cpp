7b388ddfbc0119e64def29bf0228b3e9c05c865b
kkc1383 <kangkc09@gmail.com>
2025-12-03 10:56:33 +0900

    solve 2739_B5

---
+++ 2739_B5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= 9; i++)
+	{
+		cout << n << " * " << i << " = " << n * i<<endl;
+	}
+	return 0;
+}
