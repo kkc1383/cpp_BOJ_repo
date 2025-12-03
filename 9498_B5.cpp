18ad7eb94e09c783bbd7eb2e7d5532e78c4b7810
kkc1383 <kangkc09@gmail.com>
2025-12-03 10:32:41 +0900

    solve 9498_B5

--- 9498_B5.cpp
+++ 9498_B5.cpp
@@ -5,5 +5,24 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	int score;
+
+	cin >> score;
+
+	if (score >= 90) {
+		cout << "A\n";
+	}
+	else if (score >= 80) {
+		cout << "B\n";
+	}
+	else if (score >= 70) {
+		cout << "C\n";
+	}
+	else if (score >= 60) {
+		cout << "D\n";
+	}
+	else {
+		cout << "F\n";
+	}
 	return 0;
 }
